feda036fe75947cade35fc216cbcc3882b23c9a6
DomixD <dominik-grenz@t-online.de>
2017-07-25 11:06:17 +0200

    Add start.spring.io

---
+++ src/main/java/muenchen/praxis/mfem/MfemApplication.java
@@ -0,0 +1,12 @@
+package muenchen.praxis.mfem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfemApplication.class, args);
+	}
+}
