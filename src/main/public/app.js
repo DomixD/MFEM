2008626512d8521b6805f2ed2bed46611ae9ab30
MarEliSeg <maritasegl@gmx.de>
2017-08-24 10:45:47 +0200

    Some layout tries

--- src/main/public/app.js
+++ src/main/public/app.js
@@ -1,7 +1,16 @@
-angular.module('demo', [])
-    .controller('Hello', function($scope, $http) {
+var mfem = angular.module('demo', [])
+    mfem.controller('Hello', function($scope, $http) {
         $http.get('http://localhost:8080/req').
         then(function(response) {
             $scope.req = response.data._embedded.requirements;
         });
     });
+
+mfem.controller('Hello', function($scope) {
+    $scope.title1 = 'Anforderung';
+    $scope.title4 = 'Warn';
+    $scope.isDisabled = true;
+
+    $scope.googleUrl = 'http://google.com';
+
+});
