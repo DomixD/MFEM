c5ee0ee9230c5e57c6acf26bbd7d18f63e858eb3
ThoGro <grossbec@hm.edu>
2017-08-28 10:18:14 +0200

    Implement Angular Routing

--- src/main/public/app.js
+++ src/main/public/app.js
@@ -1,7 +1,18 @@
-var mfem = angular.module('demo', [])
-    mfem.controller('Hello', function($scope, $http) {
-        $http.get('http://localhost:8080/req').
-        then(function(response) {
-            $scope.req = response.data._embedded.requirements;
+var mfem = angular.module("demo", ["ngRoute"]);
+mfem.config(function($routeProvider) {
+    $routeProvider
+
+        .when("/", {
+            templateUrl : "main.html"
+        })
+        .when("/frame", {
+            templateUrl : "frame.html"
         });
+});
+
+mfem.controller('Hello', function($scope, $http) {
+    $http.get('http://localhost:8080/req').
+    then(function(response) {
+        $scope.req = response.data._embedded.requirements;
     });
+});
