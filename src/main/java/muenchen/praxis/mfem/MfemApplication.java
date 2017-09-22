ef9f1736718de6da96fd5cfc64f320ec15850d4b
DomixD <dominik-grenz@t-online.de>
2017-09-22 08:42:41 +0200

    Update pom.xml

--- src/main/java/muenchen/praxis/mfem/MfemApplication.java
+++ src/main/java/muenchen/praxis/mfem/MfemApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EntityScan(basePackages = "muenchen.praxis.mfem")
+@ComponentScan("muenchen.praxis.mfem")
 public class MfemApplication {
 
 	public static void main(String[] args) {
