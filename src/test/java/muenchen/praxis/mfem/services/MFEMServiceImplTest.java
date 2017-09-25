7403b8e55688d5e3d0e4dea5d71a1f6574d2c805
ThoGro <grossbec@hm.edu>
2017-09-25 08:55:42 +0200

    Adapt ServiceImplTest to database

--- src/test/java/muenchen/praxis/mfem/services/MFEMServiceImplTest.java
+++ src/test/java/muenchen/praxis/mfem/services/MFEMServiceImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
@@ -33,6 +34,8 @@ public class MFEMServiceImplTest {
 
     private MockRestServiceServer server;
 
+    private List<Double> expectedList = new ArrayList<>(Arrays.asList(1.0, 0.5, 0.0, 1.0, 0.5, 0.0));
+
     @Before
     public void setUp() {
         this.server = MockRestServiceServer.createServer(restTemplate);
@@ -42,14 +45,7 @@ public class MFEMServiceImplTest {
     public void testGetResult() {
         this.server.expect(requestTo("http://localhost:8080/getRes/1/1")).andRespond(withSuccess());
         List<Double> result = this.service.getResult(1,1);
-        List<Double> expected = new ArrayList<>();
-        expected.add(1.0);
-        expected.add(0.5);
-        expected.add(0.0);
-        expected.add(1.0);
-        expected.add(0.5);
-        expected.add(0.0);
-        assertThat(result, is(equalTo(expected)));
+        assertThat(result, is(equalTo(expectedList)));
     }
 
     @Test
@@ -58,6 +54,7 @@ public class MFEMServiceImplTest {
         List<Integer> result = this.service.getFrames(1);
         List<Integer> expected = new ArrayList<>();
         expected.add(1);
+        expected.add(2);
         assertThat(result, is(equalTo(expected)));
     }
 
